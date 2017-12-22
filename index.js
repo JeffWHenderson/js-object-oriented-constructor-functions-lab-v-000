46a52e579a1b881c2919d4c5c1034bcfe7bcc552
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-22 19:22:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const Scooter = function(year, color, model) {
   this.model = model
 }
 
+const Driver = function(name, age, experience) {
+  this.name = name
+  this.age = age
+  this.experience = experience
+}
 
 // Scooter with year, color, and model properties
 // Driver with name, age, and experience properties
