0cdbd83bc24575a42e5edf075a4ce3f132efb496
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-22 19:24:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,8 @@ const Driver = function(name, age, experience) {
   this.experience = experience
 }
 
+    // don't have to call const to make constructor function. example ->
 function PickupLocation(address, city) {
   this.address = address
   this.city = city
 }
-
-// Scooter with year, color, and model properties
-// Driver with name, age, and experience properties
-// PickupLocation with address and city properties
