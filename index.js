e1ee62740ff9f0f0be9c547f5e71e5076fe8373a
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-22 19:23:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ const Driver = function(name, age, experience) {
   this.experience = experience
 }
 
+function PickupLocation(address, city) {
+  this.address = address
+  this.city = city
+}
+
 // Scooter with year, color, and model properties
 // Driver with name, age, and experience properties
 // PickupLocation with address and city properties
